c282c248cc68738f99054185ef4fee33f75451fb
SYSU-Tang <2658481919@qq.com>
2026-06-19 17:06:40 +0800

    perf:更新字符串

--- app/src/main/java/com/sysu/edu/Application.kt
+++ app/src/main/java/com/sysu/edu/Application.kt
@@ -1,19 +1,11 @@
-package com.sysu.edu;
+package com.sysu.edu
 
-import android.content.SharedPreferences;
+import android.app.Application
+import androidx.preference.PreferenceManager
+import androidx.core.content.edit
 
-import androidx.preference.PreferenceManager;
-
-import java.util.Objects;
-import java.util.Set;
-
-public class Application extends android.app.Application {
-    
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        SharedPreferences pm = PreferenceManager.getDefaultSharedPreferences(this);
-        if ((!pm.contains("dashboard")) || Objects.requireNonNull(pm.getStringSet("dashboard", null)).isEmpty())
-            pm.edit().putStringSet("dashboard", Set.of(getResources().getStringArray(R.array.values_6))).apply();
-    }
+class Application : Application() {
+	override fun onCreate() {
+		super.onCreate()
+	}
 }
