112885fee246fe804d50e25489c24e653f2f4f18
SYSU-Tang <2658481919@qq.com>
2026-07-16 00:41:33 +0800

    refactor: 使用DataClass传递参数

---
+++ app/src/main/java/com/sysu/edu/academic/CourseFilterValueData.kt
@@ -0,0 +1,42 @@
+package com.sysu.edu.academic
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize data class CourseFilterValueData(var courseName: String? = "",
+                                            var studyCampusId: String? = "",
+                                            var week: String? = "",
+                                            var classTimes: String? = "",
+                                            var courseUnitNum: String? = "",
+                                            var teachingTeacherNum: String? = "",
+                                            var teachingLanguageCode: String? = "",
+                                            var specialClassCode: String? = "") : Parcelable {
+	fun set(key: String, value: String?) {
+		when (key) {
+			"campus" -> studyCampusId = value
+			"day" -> week = value
+			"section" -> classTimes = value
+			"language" -> teachingLanguageCode = value
+			"special" -> specialClassCode = value
+		}
+	}
+}
+
+@Parcelize data class CourseFilterNameData(var courseName: String? = "",
+                                           var studyCampusId: String? = "",
+                                           var week: String? = "",
+                                           var classTimes: String? = "",
+                                           var courseUnitNum: String? = "",
+                                           var teachingTeacherNum: String? = "",
+                                           var teachingLanguageCode: String? = "",
+                                           var specialClassCode: String? = "") : Parcelable {
+	fun set(key: String, value: String?) {
+		when (key) {
+			"campus" -> studyCampusId = value
+			"day" -> week = value
+			"section" -> classTimes = value
+			"language" -> teachingLanguageCode = value
+			"special" -> specialClassCode = value
+		}
+	}
+}
