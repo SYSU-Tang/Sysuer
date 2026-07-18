d87f23e00bd58505ae3aed820ff9481e3834335d
SYSU-Tang <2658481919@qq.com>
2026-07-18 22:54:19 +0800

    chore: 构建v1.4.2-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 	defaultConfig {
 		val generation = "1"
 		val major = "4"
-		val minor = "1"
+		val minor = "2"
 		val beta = true
 		buildConfigField("int", "VERSION_GENERATION", generation)
 		buildConfigField("int", "VERSION_MAJOR", major)
