19692b662cabcfaf9072812ddcbafca914236df9
SYSU-Tang <2658481919@qq.com>
2026-08-09 17:54:40 +0800

    refactor:使用compose和viewModel重构四六级服务的界面和逻辑

--- app/src/main/java/com/sysu/edu/academic/CETActivity.kt
+++ app/src/main/java/com/sysu/edu/academic/CETActivity.kt
@@ -1,102 +1,128 @@
 package com.sysu.edu.academic
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.lifecycle.Observer
-import com.alibaba.fastjson2.JSONObject
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.ViewModelProvider
 import com.sysu.edu.BaseActivity
-import com.sysu.edu.api.CommonUtil
-import com.sysu.edu.api.CommonUtil.extractValue
-import com.sysu.edu.databinding.ActivityListBinding
-import com.sysu.edu.model.JwxtModel
-import com.sysu.edu.view.StaggeredFragment
+import com.sysu.edu.R
+import com.sysu.edu.api.DataStoreManager
+import com.sysu.edu.browser.RichTextActivity
+import com.sysu.edu.view.ActivityPager
+import com.sysu.edu.view.RowData
+import com.sysu.edu.view.SectionData
+import com.sysu.edu.view.StaggerScreen
+import com.sysu.edu.view.toMarkdown
 
 class CETActivity : BaseActivity() {
-	var page: Int = 0
-	lateinit var model: JwxtModel
-	override fun onDestroy() {
-		super.onDestroy()
-		model.dispose()
-	}
+	private val keys = listOf("考试年份",
+	                          "上/下半年",
+	                          "语言级别",
+	                          "学号",
+	                          "姓名",
+	                          "笔试考试时间",
+	                          "笔试准考证号",
+	                          "笔试成绩总分",
+	                          "听力分数",
+	                          "阅读分数",
+	                          "综合分数",
+	                          "写作分数",
+	                          "口试考试时间",
+	                          "口试准考证号",
+	                          "口语成绩",
+	                          "所属学校",
+	                          "院系",
+	                          "专业",
+	                          "年级",
+	                          "班级",
+	                          "笔试科目名称",
+	                          "笔试报名号",
+	                          "笔试报名学校",
+	                          "笔试报名校区",
+	                          "是否缺考",
+	                          "是否违纪",
+	                          "违纪类型",
+	                          "是否听力障碍",
+	                          "口试科目名称",
+	                          "口试报名号",
+	                          "口试报名学校",
+	                          "口试报名校区")
+	private val fields = arrayOf("examYear",
+	                             "thePastOrNextHalfYearName",
+	                             "languageLevel",
+	                             "stuNum",
+	                             "stuName",
+	                             "writtenExaminationTime",
+	                             "writtenExaminationNumber",
+	                             "writtenExaminationTotalScore",
+	                             "hearingScore",
+	                             "readingScore",
+	                             "comprehensiveScore",
+	                             "writingScore",
+	                             "oralExamTime",
+	                             "oralExamNumber",
+	                             "oralExamAchievement",
+	                             "schoolName",
+	                             "collegeName",
+	                             "professionName",
+	                             "grade",
+	                             "stuClassName",
+	                             "writtenExaminationSubject",
+	                             "writtenExaminationApplyNumber",
+	                             "writtenExaminationApplySchool",
+	                             "writtenExaminationApplyCampus",
+	                             "whetherMissingTest",
+	                             "whetherViolation",
+	                             "violationType",
+	                             "whetherHearingObstacle",
+	                             "oralExamSubject",
+	                             "oralExamApplyNumber",
+	                             "oralExamApplySchool",
+	                             "oralExamApplyCampus")
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		val fragment: StaggeredFragment
-		val binding = ActivityListBinding.inflate(layoutInflater).apply {
-			toolbar.setNavigationOnClickListener { supportFinishAfterTransition() }
-			fragment = list.getFragment()
-			fragment.addExportMenu(toolbar)
-		}
-		setContentView(binding.getRoot())
-		model = JwxtModel(this)
-		model.message.observe(this, Observer { message: CommonUtil.Tuple2<Int, JSONObject> ->
-			val response = message.second
-			if (response.getInteger("code") == 200) {
-				if (message.first == 0) {
-					val data = response.getJSONObject("data")
-					if (data != null) {
-						val total = data.getInteger("total")
-						var order = 1
-						data.getJSONArray("rows").forEach { a: Any? ->
-							fragment.add("${order++}",
-							             mutableListOf("考试年份", "上/下半年", "语言级别",
-							                           "学号", "姓名", "笔试考试时间",
-							                           "笔试准考证号", "笔试成绩总分",
-							                           "听力分数", "阅读分数", "综合分数",
-							                           "写作分数", "口试考试时间",
-							                           "口试准考证号", "口语成绩",
-							                           "所属学校", "院系", "专业", "年级",
-							                           "班级", "笔试科目名称",
-							                           "笔试报名号", "笔试报名学校",
-							                           "笔试报名校区", "是否缺考",
-							                           "是否违纪", "违纪类型",
-							                           "是否听力障碍", "口试科目名称",
-							                           "口试报名号", "口试报名学校",
-							                           "口试报名校区"),
-							             extractValue(a as JSONObject, arrayOf("examYear",
-							                                                   "thePastOrNextHalfYearName",
-							                                                   "languageLevel",
-							                                                   "stuNum", "stuName",
-							                                                   "writtenExaminationTime",
-							                                                   "writtenExaminationNumber",
-							                                                   "writtenExaminationTotalScore",
-							                                                   "hearingScore",
-							                                                   "readingScore",
-							                                                   "comprehensiveScore",
-							                                                   "writingScore",
-							                                                   "oralExamTime",
-							                                                   "oralExamNumber",
-							                                                   "oralExamAchievement",
-							                                                   "schoolName",
-							                                                   "collegeName",
-							                                                   "professionName",
-							                                                   "grade",
-							                                                   "stuClassName",
-							                                                   "writtenExaminationSubject",
-							                                                   "writtenExaminationApplyNumber",
-							                                                   "writtenExaminationApplySchool",
-							                                                   "writtenExaminationApplyCampus",
-							                                                   "whetherMissingTest",
-							                                                   "whetherViolation",
-							                                                   "violationType",
-							                                                   "whetherHearingObstacle",
-							                                                   "oralExamSubject",
-							                                                   "oralExamApplyNumber",
-							                                                   "oralExamApplySchool",
-							                                                   "oralExamApplyCampus")))
-						}
-						if (total > page * 10) this.exchange
-					}
-				}
-				model.nextAll()
+		enableEdgeToEdge()
+		val viewModel = ViewModelProvider(this)[CETViewModel::class.java]
+		setContent {
+			val scores by viewModel.scores.observeAsState(emptyList())
+			LaunchedEffect(Unit) {
+				viewModel.fetchNextPage()
 			}
-		})
-		exchange
-		model.next()
-	}
-	
-	val exchange: Unit
-		get() {
-			model.add("jwxt/achievement-manage/englishGradeAchievement/stuPageList",
-			          "{\"pageNo\":${++page},\"pageSize\":10,\"total\":true,\"param\":{}}", 0)
+			val sections = remember(scores) {
+				val snapshotList = mutableStateListOf<SectionData>()
+				scores.forEachIndexed { index, score ->
+					val rows = mutableStateListOf<RowData>()
+					keys.forEachIndexed { i, key ->
+						rows.add(RowData(key, score.getString(fields[i])))
+					}
+					snapshotList.add(SectionData(title = "${index + 1}", rows = rows))
+				}
+				snapshotList
+			}
+			ActivityPager(title = stringResource(R.string.cet), onNavigationClick = { supportFinishAfterTransition() }, actions = {
+				IconButton(onClick = {
+					val markdown = sections.toMarkdown()
+					DataStoreManager.saveContent(this@CETActivity, getString(R.string.cet), markdown) {
+						startActivity(Intent(this@CETActivity, RichTextActivity::class.java).putExtra("type", DataStoreManager.ContentType.MARKDOWN.name).putExtra("title", getString(R.string.cet))/*,
+					              ActivityOptionsCompat.makeSceneTransitionAnimation(this@CETActivity, window.decorView, "miniapp").toBundle()*/)
+					}
+				}) {
+					Icon(painter = painterResource(id = R.drawable.export), contentDescription = stringResource(R.string.export))
+				}
+			}, isNestedScrollEnabled = false, pageContent = {
+				StaggerScreen(sections = sections, onScrollBottom = { viewModel.fetchNextPage() })
+			})
 		}
+	}
 }
