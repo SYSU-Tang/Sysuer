06e909f02e7466d7a54b9e520d173cffad03b8f5
SYSU-Tang <2658481919@qq.com>
2026-08-27 19:42:55 +0800

    chore:升级Gradle和部分依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,10 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.google.gms.google.services) apply false
     alias(libs.plugins.google.firebase.crashlytics) apply false
     kotlin("jvm") version "2.4.0" apply false
     id("com.google.devtools.ksp") version "2.3.9" apply false
-    id("androidx.room3") version "3.0.0" apply false
+    id("androidx.room3") version "3.0.1" apply false
 }
