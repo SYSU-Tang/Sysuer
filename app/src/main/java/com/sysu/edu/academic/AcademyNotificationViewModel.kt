5a436116c4e2421859cdf35a9c60a2f57f524c42
SYSU-Tang <2658481919@qq.com>
2026-09-05 17:30:48 +0800

    perf:教务通知添加搜索功能

--- app/src/main/java/com/sysu/edu/academic/AcademyNotificationViewModel.kt
+++ app/src/main/java/com/sysu/edu/academic/AcademyNotificationViewModel.kt
@@ -8,54 +8,68 @@ import com.alibaba.fastjson2.JSONObject
 import com.sysu.edu.model.JwxtModel
 
 class AcademyNotificationViewModel(application: Application) : AndroidViewModel(application) {
-    private val model: JwxtModel = JwxtModel(application)
-    
-    private val _academicNotices = MutableLiveData<List<JSONObject>>()
-    val academicNotices: LiveData<List<JSONObject>> = _academicNotices
-    
-    private val _schoolNotices = MutableLiveData<List<JSONObject>>()
-    val schoolNotices: LiveData<List<JSONObject>> = _schoolNotices
-    
-    private val _noticeContent = MutableLiveData<String?>()
-    val noticeContent: LiveData<String?> = _noticeContent
+	private val model: JwxtModel = JwxtModel(application)
 
-    init {
-        model.message.observeForever { (code, response) ->
-            if (response.getInteger("code") == 200) {
-                when (code) {
-                    0 -> {
-                        val list = response.getJSONObject("data").getJSONArray("list")
-                        _academicNotices.postValue(list.filterIsInstance<JSONObject>())
-                    }
-                    1 -> {
-                        val list = response.getJSONObject("data").getJSONArray("list")
-                        _schoolNotices.postValue(list.filterIsInstance<JSONObject>())
-                    }
-                    2 -> {
-                        val data = response.getString("data")
-                        _noticeContent.postValue(data)
-                    }
-                }
-                model.nextAll()
-            }
-        }
-    }
+	private val _academicNotices = MutableLiveData<List<JSONObject>>()
+	val academicNotices: LiveData<List<JSONObject>> = _academicNotices
 
-    fun fetchNotices() {
-        model.add("jwxt/system-manage/info-delivery?column=01&deliveryObject=02&status=1&resourceCode=jwgld", 0)
-        model.add("jwxt/system-manage/info-delivery?column=02&deliveryObject=02&status=1&resourceCode=jwgld", 1)
-        model.next()
-    }
+	private val _schoolNotices = MutableLiveData<List<JSONObject>>()
+	val schoolNotices: LiveData<List<JSONObject>> = _schoolNotices
 
-    fun fetchContent(id: String) {
-        model.addAndNext("jwxt/system-manage/info-delivery/noticeId?id=$id", 2)
-    }
+	private val _noticeContent = MutableLiveData<String?>()
+	val noticeContent: LiveData<String?> = _noticeContent
 
-    fun clearNoticeContent() {
-        _noticeContent.value = null
-    }
+	init {
+		model.message.observeForever { (code, response) ->
+			if (response.getInteger("code") == 200) {
+				when (code) {
+					0 -> {
+						val list = response.getJSONObject("data").getJSONArray("list")
+						_academicNotices.postValue(list.filterIsInstance<JSONObject>())
+					}
 
-    override fun onCleared() {
-        model.dispose()
-    }
+					1 -> {
+						val list = response.getJSONObject("data").getJSONArray("list")
+						_schoolNotices.postValue(list.filterIsInstance<JSONObject>())
+					}
+
+					2 -> {
+						val data = response.getString("data")
+						_noticeContent.postValue(data)
+					}
+				}
+			}
+		}
+	}
+
+	fun fetchNotices() {
+		fetchAcademicNotice()
+		fetchSchoolNotice()
+	}
+
+	fun fetchAcademicNotice(keyword: String? = null) {
+		model.addAndNext(
+			"jwxt/system-manage/info-delivery?column=01&deliveryObject=02&status=1&resourceCode=jwgld&title=$keyword",
+			0
+		)
+	}
+
+	fun fetchSchoolNotice(keyword: String? = null) {
+		model.addAndNext(
+			"jwxt/system-manage/info-delivery?column=02&deliveryObject=02&status=1&resourceCode=jwgld&title=$keyword",
+			1
+		)
+	}
+
+	fun fetchContent(id: String) {
+		model.addAndNext("jwxt/system-manage/info-delivery/noticeId?id=$id", 2)
+	}
+
+	fun clearNoticeContent() {
+		_noticeContent.value = null
+	}
+
+	override fun onCleared() {
+		model.dispose()
+	}
 }
